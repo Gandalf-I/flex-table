f6bb22b36d02e68e91ad3f82a649e5ac0cfb8f0f
danyl <danylaverbah@gmail.com>
2021-09-05 15:19:11 +0300

    refactoring

--- src/app/pages/publication/table-settings/table-settings.component.spec.ts
+++ src/app/pages/publication/table-settings/table-settings.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TableSettingsComponent } from './table-settings.component';
 
@@ -6,7 +6,7 @@ describe('TableSettingsComponent', () => {
   let component: TableSettingsComponent;
   let fixture: ComponentFixture<TableSettingsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [TableSettingsComponent],
     })
--- src/app/shared/components/flex-inputs/flex-inputs.component.spec.ts
+++ src/app/shared/components/flex-inputs/flex-inputs.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FlexInputsComponent } from './flex-inputs.component';
 
@@ -6,7 +6,7 @@ describe('FlexInputsComponent', () => {
   let component: FlexInputsComponent;
   let fixture: ComponentFixture<FlexInputsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [FlexInputsComponent],
     })
